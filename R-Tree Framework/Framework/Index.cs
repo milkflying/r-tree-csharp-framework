f2f3b163d6f5ce2e3d5d121f5ad9d23ad440d566
galacticb <galacticb@c2b99a85-4940-0410-bb0c-158b58bef44a>
2007-12-02 15:15:27 +0000

    Test Commit

--- R-Tree Framework/Framework/Index.cs
+++ R-Tree Framework/Framework/Index.cs
@@ -9,6 +9,6 @@ namespace Edu.Psu.Cse.R_Tree_Framework.Framework
         void Insert(Record record);
         void Delete();
         void Update();
-        List<Record> Search(Query query);
+        List<Record> Search(Query query); 
     }
 }
