9bf4916d4b19f64ce2e258ee63733054ec958dbf
ArcusAngelus <ArcusAngelus@c2b99a85-4940-0410-bb0c-158b58bef44a>
2007-12-04 23:38:58 +0000

    Completed LRU Cache Manager

--- trunk/R-Tree Framework/Framework/Node.cs
+++ trunk/R-Tree Framework/Framework/Node.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace Edu.Psu.Cse.R_Tree_Framework.Framework
 {
-    public class Node
+    public class Node : PageData
     {
         protected List<NodeEntry> nodeEntries;
         protected Guid address, parent;
