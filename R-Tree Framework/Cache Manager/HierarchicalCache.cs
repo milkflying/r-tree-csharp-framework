2a4c6a4626822296f0c2c4d5353ef0bc809387ec
arcusangelus <arcusangelus@c2b99a85-4940-0410-bb0c-158b58bef44a>
2008-03-24 07:51:02 +0000

    Removed Generic CoordinateType and replaced with Single

--- R-Tree Framework/Cache Manager/HierarchicalCache.cs
+++ R-Tree Framework/Cache Manager/HierarchicalCache.cs
@@ -5,7 +5,7 @@ using R_Tree_Framework.Framework;
 
 namespace R_Tree_Framework.Cache_Manager
 {
-    public class HierarchicalCache<CoordinateType> //: CacheManager<CoordinateType> where CoordinateType : struct, IComparable
+    public class HierarchicalCache : CacheManager
     {
     }
 }
